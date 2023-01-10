854e460c9c55e7a78bfbe805b974be8d6dbf83aa
AngeloCalabro <angelo.1991@hotmail.com>
2023-01-10 16:59:08 +0100

    route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +24,9 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')
    ->group(function () {
          Route::get('/', [DashboardController::class, 'index'])
          ->name('dashboard');
+        Route::resource('projects', ProjectController::class);
    });
+
+
 
 require __DIR__.'/auth.php';
